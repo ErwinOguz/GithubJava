289bd65e2a3ac1a5e975bffc29885dd3e7151424
ErwinOguz <w89872jo>
2015-02-17 15:53:43 +0000

    Initial commit

---
+++ de.vogella.git.first/de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.vogella.git.first;
+
+/**
+ * @author EO
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
