9ca55e8719f0667af594fb26a3556670ae7ebfd9
Oguz, Erwin <erwin.oguz@gmail.com>
2017-01-30 20:52:05 +0000

    eoGradleGI_AI

---
+++ eoGradleGI_AI_ModName/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by EO on 29/01/2017.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("hello, world");
+    }
+}
---
+++ eoGradleGI_AI_ModName/src/test/java/HelloWorldTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by EO on 29/01/2017.
+ */
+public class HelloWorldTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
